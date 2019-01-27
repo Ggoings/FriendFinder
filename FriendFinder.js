f9dd49abbaebfd69c9d3840722c216a1ec085387
Ggoings <42050294+Ggoings@users.noreply.github.com>
2019-01-27 07:27:32 -0600

    Add files via upload

---
+++ FriendFinder.js
@@ -0,0 +1,8 @@
+The Friend Finder app will ask 10 questions. Each answer will be on a scale of 1 to 5 based on how much the user answers the questions.
+
+Determine the user's most compatible friends.
+Each user's answers will display as numbers 1-5.
+Then it'll compare the difference between current user's scores against those from other users.
+The closest match will be the user with the least amount of difference.
+
+Once its found the current user's most compatible friend, a window will display the result with a photo and name of the closest match.
